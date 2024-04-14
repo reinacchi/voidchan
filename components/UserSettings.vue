62df62c875864fddd7d798fb1393da8dfee1dfab
Reinhardt <notmarx.tech@gmail.com>
2024-04-14 10:54:06 +0700

    Add Authentication tab

--- components/UserSettings.vue
+++ components/UserSettings.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <NuxtSnackBar />
     <h1 class="text-5xl m-5 noselect">Settings</h1>
+    <br class="noselect">
     <center>
       <div class="w-full max-w-xs">
         <form class="shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -19,28 +19,36 @@
               placeholder="Password"
             />
           </div>
+          <button class="btn mx-2 noselect" @click.prevent="updateSettings">Update</button>
           </form>
           </div>
+          <div class="w-full max-w-sm shadow-md rounded px-8 pt-6 pb-8 mb-8 noselect">
+            <label class="block text-lg font-bold mb-3 noselect">Authentiation Key</label>
+            <button @click="copyAuth" class="btn mx-2">{{ copiedAuth ? "Copied to Clipboard!" : "Copy Key" }}</button>
+            <button style="border-color: #ad0c00;" class="btn mx-2">Reset Key</button>
+          </div>
           <button class="btn mx-2 noselect" @click.prevent="$router.go(-1)">Back</button>
-          <button class="btn mx-2 noselect" @click.prevent="updateSettings">Update</button>
     </center>
   </div>
 </template>
 
 <script setup lang="ts">
-const config = useRuntimeConfig();
+const config = useAppConfig();
 const password = ref(undefined);
-const newName = ref(undefined);
-const newPassword = ref(undefined);
+const copiedAuth = ref(false);
 const { data } = useSession();
 const { data: userProfile } = useFetch(`/api/users/${data.value?.user?.name}`, {
   method: "GET",
   headers: {
-    authorization: "internalsecretkeylololol"
+    authorization: config.PrivateAuth,
   }
 });
 
 function updateSettings() {
+  if (password.value === undefined) {
+    return;
+  }
+
   $fetch(`/api/users/${data.value?.user?.name}`, {
     method: "PATCH",
     body: JSON.stringify({
@@ -48,24 +56,16 @@ function updateSettings() {
     })
   });
 
-
   password.value = undefined;
 }
 
-function createUser() {
-  if (!newName.value && !newPassword.value) {
-    return;
-  }
+function copyAuth() {
+  navigator.clipboard.writeText(userProfile.value.auth).then(() => {
+    copiedAuth.value = true;
 
-  $fetch(`/api/users/create`, {
-    method: "POST",
-    body: JSON.stringify({
-      name: newName.value,
-      password: newPassword.value
-    })
-  });
-
-  newName.value = undefined;
-  newPassword.value = undefined;
+    setTimeout(() => {
+      copiedAuth.value = false;
+    }, 1500)
+  })
 }
 </script>
