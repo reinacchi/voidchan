b1a1671724a5202e4ec501cc1970438b802059e7
Reinhardt <notmarx.tech@gmail.com>
2024-05-06 11:26:34 +0700

    Reformat code

--- components/FileGridGallery.vue
+++ components/FileGridGallery.vue
@@ -1,28 +1,61 @@
 <template>
-<div v-if="!files">
-  <NotFound />
-</div>
-<div class="hidden md:block">
-  <div class="gallery-view noselect" v-for="file in files" :key="file.id">
-    <nuxt-link v-if="file.nsfw" :href="'/v/' + file.id + '.' + file.ext"><Image style="cursor: pointer;" class="nsfw" :src="file.url" draggable="false" alt="" /></nuxt-link>
-    <nuxt-link v-else :href="'/v/' + file.id + '.' + file.ext"><Image style="cursor: pointer;" :src="file.url" draggable="false" alt="" /></nuxt-link>
+  <div v-if="!files">
+    <NotFound />
   </div>
-</div>
-<div class="md:hidden">
-  <div class="gallery-view-mb noselect md:hidden" v-for="file in files" :key="file.id">
-    <nuxt-link v-if="file.nsfw" :href="'/v/' + file.id + '.' + file.ext"><Image style="cursor: pointer;" class="nsfw" :src="file.url" draggable="false" alt="" /></nuxt-link>
-    <nuxt-link v-else :href="'/v/' + file.id + '.' + file.ext"><Image style="cursor: pointer;" :src="file.url" draggable="false" alt="" /></nuxt-link>
+  <div class="hidden md:block">
+    <div class="gallery-view noselect" v-for="file in files" :key="file.id">
+      <nuxt-link v-if="file.nsfw" :href="'/v/' + file.id + '.' + file.ext"
+        ><Image
+          style="cursor: pointer"
+          class="nsfw"
+          :src="file.url"
+          draggable="false"
+          alt=""
+      /></nuxt-link>
+      <nuxt-link v-else :href="'/v/' + file.id + '.' + file.ext"
+        ><Image
+          style="cursor: pointer"
+          :src="file.url"
+          draggable="false"
+          alt=""
+      /></nuxt-link>
+    </div>
   </div>
-</div>
+  <div class="md:hidden">
+    <div
+      class="gallery-view-mb noselect md:hidden"
+      v-for="file in files"
+      :key="file.id"
+    >
+      <nuxt-link v-if="file.nsfw" :href="'/v/' + file.id + '.' + file.ext"
+        ><Image
+          style="cursor: pointer"
+          class="nsfw"
+          :src="file.url"
+          draggable="false"
+          alt=""
+      /></nuxt-link>
+      <nuxt-link v-else :href="'/v/' + file.id + '.' + file.ext"
+        ><Image
+          style="cursor: pointer"
+          :src="file.url"
+          draggable="false"
+          alt=""
+      /></nuxt-link>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 const user = useRoute().params.user;
-const { data: files } = await useAsyncData("files", () => $fetch(`/api/users/${user}/files`));
+const { data: files } = await useAsyncData("files", () =>
+  $fetch(`/api/users/${user}/files`)
+);
 </script>
 
 <style scoped>
-.gallery-view {
+.gallery-view,
+.gallery-view-mb {
   max-width: 20%;
   border: none !important;
   box-shadow: 0 0 4px 2px rgb(60, 27, 109);
@@ -34,35 +67,17 @@ const { data: files } = await useAsyncData("files", () => $fetch(`/api/users/${u
 
 .gallery-view-mb {
   max-width: 40%;
-  border: none !important;
-  box-shadow: 0 0 4px 2px rgb(60, 27, 109);
-  margin: 20px;
-  display: flex;
-  display: inline-block;
-  transition: all cubic-bezier(0.175, 0.885, 0.32, 1.275) 0.3s;
 }
 
-.gallery-view:hover {
-  box-shadow: 0 0 10px 5px rgb(60, 27, 109);
-  border: 20px;
-  color: rgb(60, 27, 109);
-  transform: translateY(-0.30em);
-}
-
-.gallery-view img {
-  max-width: 100%;
-  height: auto;
-  object-fit: contain;
-  vertical-align: auto;
-}
-
+.gallery-view:hover,
 .gallery-view-mb:hover {
   box-shadow: 0 0 10px 5px rgb(60, 27, 109);
   border: 20px;
   color: rgb(60, 27, 109);
-  transform: translateY(-0.30em);
+  transform: translateY(-0.3em);
 }
 
+.gallery-view img,
 .gallery-view-mb img {
   max-width: 100%;
   height: auto;
