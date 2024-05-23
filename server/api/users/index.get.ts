1147157f00ee754b86d4a5b20bc3dfdcec10d684
Reinhardt <notmarx.tech@gmail.com>
2024-05-23 10:54:07 +0700

    Update code

--- server/api/users/index.get.ts
+++ server/api/users/index.get.ts
@@ -7,58 +7,62 @@ export default defineEventHandler(async (event) => {
   const users = (await conn.query("SELECT * FROM users")) as IUser[];
 
   if (event.node.req.headers.authorization === config.PrivateAuth) {
-    const promises = await users.map(async (user) => {
-      return conn.query(
+    const promises = users.map(async (user) => {
+      const userFiles = await conn.query(
         "SELECT * FROM files WHERE uploader = ?",
         [user.username]
-      ).then((userFiles) => {
-        return conn.query(
-          "SELECT * FROM posts WHERE uploader = ?",
-          [user.username]
-        ).then((userPosts) => {
-          return {
-            id: user.id,
-            clearanceLevels: JSON.parse(user.clearanceLevels) as string[],
-            createdAt: user.createdAt,
-            auth: user.authKey,
-            email: user.email,
-            displayName: user.displayName,
-            username: user.username,
-            kudos: user.kudos,
-            files: userFiles.length as number,
-            posts: userPosts.length as number,
-          };
-        });
-      });
+      );
+
+      const userPosts = await conn.query(
+        "SELECT * FROM posts WHERE uploader = ?",
+        [user.username]
+      );
+
+      return {
+        id: user.id,
+        clearanceLevels: JSON.parse(user.clearanceLevels) as string[],
+        createdAt: user.createdAt,
+        auth: user.authKey,
+        email: user.email,
+        displayName: user.displayName,
+        username: user.username,
+        kudos: user.kudos,
+        files: userFiles.length as number,
+        posts: userPosts.length as number,
+      };
     });
-    const data = await Promise.all(promises).then((res) => res);
+
+    const data = await Promise.all(promises);
 
     return data;
+
   } else {
-    const promises = await users.map(async (user) => {
-      return conn.query(
+    const promises = users.map(async (user) => {
+      const userFiles = await conn.query(
         "SELECT * FROM files WHERE uploader = ?",
         [user.username]
-      ).then((userFiles) => {
-        return conn.query(
-          "SELECT * FROM posts WHERE uploader = ?",
-          [user.username]
-        ).then((userPosts) => {
-          return {
-            id: user.id,
-            clearanceLevels: JSON.parse(user.clearanceLevels) as string[],
-            createdAt: user.createdAt,
-            displayName: user.displayName,
-            username: user.username,
-            kudos: user.kudos,
-            files: userFiles.length as number,
-            posts: userPosts.length as number,
-          };
-        });
-      });
+      );
+
+      const userPosts = await conn.query(
+        "SELECT * FROM posts WHERE uploader = ?",
+        [user.username]
+      );
+
+      return {
+        id: user.id,
+        clearanceLevels: JSON.parse(user.clearanceLevels) as string[],
+        createdAt: user.createdAt,
+        displayName: user.displayName,
+        username: user.username,
+        kudos: user.kudos,
+        files: userFiles.length as number,
+        posts: userPosts.length as number,
+      };
     });
-    const data = await Promise.all(promises).then((res) => res);
+
+    const data = await Promise.all(promises);
 
     return data;
+
   }
 });
