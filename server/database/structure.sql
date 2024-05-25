3ce999d1f36b93297608431158e14c2e3838b3af
Reinhardt <notmarx.tech@gmail.com>
2024-05-25 22:41:11 +0700

    Cleanup code

--- server/database/structure.sql
+++ server/database/structure.sql
@@ -13,26 +13,32 @@ CREATE TABLE posts (
     createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     buffer LONGTEXT NOT NULL,
     mimetype VARCHAR(255) NOT NULL,
-		uploader VARCHAR(255) NOT NULL,
-		status ENUM("approved", "pending", "disapproved") DEFAULT "approved" NOT NULL,
-		tags JSON,
-		comments JSON,
-		favourites INT DEFAULT 0,
-		source JSON,
-		size INT NOT NULL,
-		rating ENUM("explicit", "erotica", "suggestive", "safe") NOT NULL,
-		PRIMARY KEY (id)
+    uploader VARCHAR(255) NOT NULL,
+    status ENUM("approved", "pending", "disapproved") DEFAULT "approved" NOT NULL,
+    tags JSON,
+    comments JSON,
+    favourites INT DEFAULT 0,
+    source JSON,
+    size INT NOT NULL,
+    rating ENUM("explicit", "erotica", "suggestive", "safe") NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE tags (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(255) UNIQUE NOT NULL,
-	category ENUM("general", "meta", "character", "copyright", "artist") NOT NULL,
-	createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	updatedAt DATETIME NOT NULL,
-	description TEXT,
-	aliases JSON,
-	creatorID VARCHAR(255) NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    category ENUM(
+        "general",
+        "meta",
+        "character",
+        "copyright",
+        "artist"
+    ) NOT NULL,
+    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME NOT NULL,
+    description TEXT,
+    aliases JSON,
+    creatorID VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE users (
